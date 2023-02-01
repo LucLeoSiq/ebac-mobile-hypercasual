ee0ed3bc1a8ba471ab4fbc4c3417d710fce095b6
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-02-01 08:47:00 -0300

    feat: add enemy and player stop on collision

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    [Header("Lerp")]
+    public Transform lerpTarget;
+    public float lerpSpeed = 1f;
+
+    [Header("Player Speed")]
     public float forwardSpeed = 1f;
 
-    void Start()
+    [Header("Tags Setup")]
+    public string tagToCheckEnemy = "Enemy";
+
+    private bool _canRun;
+    private Vector3 _pos;
+
+    private void Start()
     {
-        
+        _canRun = true;
     }
 
     void Update()
     {
-        transform.Translate(transform.forward  * forwardSpeed * Time.deltaTime); 
+        if (_canRun)
+        {
+            _pos = lerpTarget.position;
+            _pos.y = transform.position.y;
+            _pos.z = transform.position.z;
+
+            transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
+            transform.Translate(transform.forward * forwardSpeed * Time.deltaTime);
+        } 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == tagToCheckEnemy)
+        {
+            _canRun = false;
+        } 
     }
 }
