eff015c6472f5ee6a1d1f23845b594816b9dc9e7
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-02-10 13:45:41 -0300

    feat: coins disappear after touching player

--- Assets/Scripts/Items/ItemCollectableCoin.cs
+++ Assets/Scripts/Items/ItemCollectableCoin.cs
@@ -8,12 +8,18 @@ public class ItemCollectableCoin : ItemCollectableBase
     public Collider collider;
     public bool collect = false;
     public float lerp = 5f;
+    public float minDistance = 1f;
 
     private void Update()
     {
         if (collect)
         {
             transform.position = Vector3.Lerp(transform.position, PlayerController.Instance.transform.position, lerp * Time.deltaTime);
+
+            if(Vector3.Distance(transform.position, PlayerController.Instance.transform.position) < minDistance)
+            {
+                HideItens();
+            }
         }
     }
 
