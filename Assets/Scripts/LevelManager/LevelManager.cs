02fa3db7142041503c99d850032da37281ae7403
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-02-17 21:37:27 -0300

    feat: adjust script so level position resets to 0 when loaded

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -6,7 +6,9 @@ public class LevelManager : MonoBehaviour
 {
     public Transform container;
 
-    public GameObject level;
+    public List<GameObject> level;
+
+    [SerializeField] private int _index;
 
     private void Awake()
     {
@@ -15,6 +17,7 @@ public class LevelManager : MonoBehaviour
 
     private void SpawnLevel()
     {
-        var currentLevel = Instantiate(level, container);
+        var currentLevel = Instantiate(level[_index], container);
+        currentLevel.transform.localPosition = Vector3.zero;
     }
 }
