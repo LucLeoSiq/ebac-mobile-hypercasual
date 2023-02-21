e7d0bb5a836221757cdcd09b4eec7a8d47950749
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-02-21 09:43:48 -0300

    chore: update main branch

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class LevelManager : MonoBehaviour
 {
@@ -17,6 +18,11 @@ public class LevelManager : MonoBehaviour
 
     [SerializeField] private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
     private LevelPieceBasedSetup _currSetup;
+
+    [Header("Animation")]
+    public float scaleDuration = .2f;
+    public float scaleTimeBetweenPieces;
+    public Ease ease = Ease.OutBack;
 
     private void Awake()
     {
@@ -78,9 +84,25 @@ public class LevelManager : MonoBehaviour
 
         ColorManager.Instance.ChangeColorByType(_currSetup.artType);
 
+        StartCoroutine(ScalePiecesByTime()); 
     }
 
-  
+    IEnumerator ScalePiecesByTime()
+    {
+        foreach(var p in _spawnedPieces)
+        {
+            p.transform.localScale = Vector3.zero;
+        }
+
+        yield return null;
+
+        for(int i= 0; i < _spawnedPieces.Count; i++)
+        {
+            _spawnedPieces[i].transform.DOScale(1, scaleDuration).SetEase(ease);
+            yield return new WaitForSeconds(timeBetweenPieces);
+        }
+    }
+
     private void CreateLevelPiece(List<LevelPieceBase> list)
     {
         var piece = list[Random.Range(0, list.Count)];
@@ -122,7 +144,7 @@ public class LevelManager : MonoBehaviour
         for (int i = 0; i < _currSetup.piecesNumber; i++)
         {
             CreateLevelPiece(_currSetup.levelPieces);
-            yield return new WaitForSeconds(timeBetweenPieces);
+            yield return new WaitForSeconds(scaleTimeBetweenPieces);
         }
     }
 
