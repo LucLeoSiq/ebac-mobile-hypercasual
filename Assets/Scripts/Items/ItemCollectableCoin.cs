16d7b7f1adbed2dc01136664a98eaa8d8fb51404
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-02-21 19:14:21 -0300

    feat: add character animation when collecting coins

--- Assets/Scripts/Items/ItemCollectableCoin.cs
+++ Assets/Scripts/Items/ItemCollectableCoin.cs
@@ -34,6 +34,7 @@ public class ItemCollectableCoin : ItemCollectableBase
         base.OnCollect();
         collider.enabled = false;
         collect = true;
+        PlayerController.Instance.Bounce();
     }
 
     protected override void Collect()
