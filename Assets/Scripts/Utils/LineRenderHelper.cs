c999955398a0390da9dd856a1dc019d2b03a991b
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-02-23 10:51:08 -0300

    feat: testing line renderer on cubes

---
+++ Assets/Scripts/Utils/LineRenderHelper.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRenderHelper : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+
+    public List<Transform> positions;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer.positionCount = positions.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < positions.Count; i++)
+        {
+            lineRenderer.SetPosition(i, positions[i].position);
+        }
+    }
+}
